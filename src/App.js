eace463a6512518d17e96add4101ada019926b82
Shrdul <deshmukh.shard@northeastern.edu>
2023-10-16 01:49:21 -0400

    Project Initialized

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import react from "react";
+import { BrowserRouter } from 'react-router-dom';
+import Homeq from './Pages/Home/Homescreen';
+import Home from './Pages/Home/Homescreen';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
