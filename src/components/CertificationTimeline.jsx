743375a4806e94f4a2d2ea8b0917631687c1d944
Shrdul <deshmukh.shard@northeastern.edu>
2025-03-23 13:24:42 -0400

    Added certification images

--- src/components/CertificationTimeline.jsx
+++ src/components/CertificationTimeline.jsx
@@ -1,5 +1,5 @@
 // components/ExperienceTimeline.js
-import React from 'react';
+import React from 'react'; // Removed useEffect since we're not using Credly's script
 import '../css/Timeline.css';
 
 const experienceData = [
@@ -7,25 +7,29 @@ const experienceData = [
     year: 'AWS',
     title: 'Solutions Architect Associate(SAA-003)',
     company: 'Validation Number : 66HREWSK82B4QZKZ',
-    details: 'Validate At : https://aws.amazon.com/verification',
+    badgeId: '9b720383-1c99-486d-b87c-0d6c94861c27',
+    badgeImage: 'https://images.credly.com/size/340x340/images/0e284c3f-5164-4b21-8660-0d84737941bc/image.png',
   },
   {
     year: '',
     title: 'Certified Cloud Practitioner',
     company: 'Validation Number : MCDQM91LJ1BQ1Z9N',
-    details: 'Validate At : https://aws.amazon.com/verification',
+    badgeId: 'beebce4b-e820-4a3f-b95d-8c9cc886acb8',
+    badgeImage: 'https://images.credly.com/size/340x340/images/00634f82-b07f-4bbd-a6bb-53de397fc3a6/image.png'
   },
   {
     year: 'HashiCorp Cloud Certifications',
     title: 'Terraform Associate (HCTAO-003)',
     company: 'Certificate Number : 2e7797ba-b78b-4695-9260-e389b5ca7328',
-    details: 'Validate At : https://www.credly.com/badges/2e7797ba-b78b-4695-9260-e389b5ca7328/public_url',
+    badgeId: '2e7797ba-b78b-4695-9260-e389b5ca7328',
+    badgeImage: 'https://images.credly.com/size/340x340/images/99289602-861e-4929-8277-773e63a2fa6f/image.png', // Replace with actual image URL
   },
   {
     year: 'Microsoft',
     title: 'Azure Fundamentals',
     company: 'Certification Number : I791-6335',
-    details: 'Validate At : https://www.credly.com/badges/115ab97f-a350-481f-a808-b6fa33ed0288/public_url',
+    badgeId: '115ab97f-a350-481f-a808-b6fa33ed0288',
+    badgeImage: 'https://images.credly.com/size/340x340/images/be8fcaeb-c769-4858-b567-ffaaa73ce8cf/image.png', // Replace with actual image URL
   },
 ];
 
@@ -37,10 +41,26 @@ const CertificationTimeline = () => {
           <div key={index} className="timeline-item">
             <div className="timeline-year">{item.year}</div>
             <div className="timeline-content">
-              <div className="timeline-box">
-                <h3>{item.title}</h3>
-                <h4>{item.company}</h4>
-                <p>{item.details}</p>
+              <div className="timeline-box timeline-flex">
+                {item.badgeId && item.badgeImage && (
+                  <div className="credly-badge">
+                    <a 
+                      href={`https://www.credly.com/badges/${item.badgeId}/public_url`} 
+                      target="_blank" 
+                      rel="noopener noreferrer"
+                    >
+                      <img 
+                        src={item.badgeImage} 
+                        alt={item.title} 
+                      />
+                    </a>
+                  </div>
+                )}
+                <div>
+                  <h3>{item.title}</h3>
+                  <h4>{item.company}</h4>
+                  <p>{item.details}</p>
+                </div>
               </div>
             </div>
           </div>
