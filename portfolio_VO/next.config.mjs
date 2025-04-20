31a65b1062ed53cf72d83c833f2622ecb5e94b71
Shrdul <deshmukh.shard@northeastern.edu>
2025-04-20 18:15:23 -0400

    updated workflow

--- portfolio_VO/next.config.mjs
+++ portfolio_VO/next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Enables static export
-  trailingSlash: true, // Ensures folders have trailing slashes for static hosting
+  trailingSlash: true, // GitHub Pages requires folders to have index.html
+  basePath: '/Portfolio-Website',
+  assetPrefix: '/Portfolio-Website/',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,7 +11,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true, // Required for static export with images
+    unoptimized: true,
   },
 };
 
